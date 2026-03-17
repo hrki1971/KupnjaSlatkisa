45bb88c963d425489dec1e39697be3758ab1820b
hrki1971 <h.mitric@gmail.com>
2026-03-17 18:52:27 +0100

    slatkiši

--- src/pages/kategorije/KategorijaPregled.jsx
+++ src/pages/kategorije/KategorijaPregled.jsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from "react"
+import KategorijeServis from "../../service/kategorije/KategorijeService"
+
 export default function KategorijaPregled(){
+
+    const [kategorije,setKategorije]=useState([])
+
+    useEffect(()=>{
+        ucitajKategorije()
+
+    },[])
+    async function ucitajKategorije(){
+        await <KategorijeServis.get().then((odgovor)=>{
+            setKategorije(odgovor.data)
+        })
+    }
     return(
         <>
+        <ul>
+            {kategorije && kategorije.map((kategorija)=>(
+                <li>{kategorija.naziv}</li>
+            ))}
+        </ul>
         Ovdje će se vidjeti kategorije
         </>
     )
