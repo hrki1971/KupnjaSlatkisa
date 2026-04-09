9e3f292cf826e92565860edd7137274e53fbcaf4
hrki1971 <h.mitric@gmail.com>
2026-04-09 15:04:46 +0200

    slatkiši dodano

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,54 @@
 import { IME_APLIKACIJE } from "../constants";
 import slika from '../assets/slika.jpg';
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+import { Card, Col, Row } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import KategorijaService from "../service/kategorije/KategorijaService";
+import SlatkisiService from "../service/slatkisi/SlatkisiService";
 
 export default function Home(){
+    const [brojKategorija, setBrojKategorija] = useState(0);
+    const [brojSlatkisa, setBrojSlatkisa] = useState(0);
+    const [animatedKategorije, setAnimatedKategorije] = useState(0);
+    const [animatedSlatkisi, setAnimatedSlatkisi] = useState(0);
+    
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const kategorijeRezultat = await KategorijaService.get();
+                const slatkisi = await SlatkisiService.get();
+
+                setBrojKategorija(kategorijeRezultat.length);
+                setBrojSlatkisa(slatkisi.length);
+            } catch (error) {
+                console.error("Greška pri dohvaćanju podataka:", error);    
+            }
+        };
+        fetchData();
+    },[]);
+
+    useEffect(() => {
+        if(animatedKategorije < brojKategorija) {
+            const timer = setTimeout(() => {
+                setAnimatedKategorije(prev => Math.min(prev + 1, brojKategorija));
+            }, 300);
+            return () => clearTimeout(timer);
+        }
+    }, [animatedKategorije, brojKategorija]);
+
+    useEffect(() => {
+        if(animatedSlatkisi < brojSlatkisa) {
+            const timer = setTimeout(() => {
+                setAnimatedSlatkisi(prev => Math.min(prev + 1, brojSlatkisa));
+            }, 200);
+            return () => clearTimeout(timer);
+        }
+    }, [animatedSlatkisi, brojSlatkisa]);
+
     return(
     <>
+    <Row>
+    <Col md={6}>
     <div style={{ textAlign:'center', marginTop:'4 rem'}}>
         <img src={slika} alt="slika" />
 
@@ -22,6 +66,41 @@ export default function Home(){
         />
         
     </div>
+    </Col> 
+    <Col className="d-flex align-items-center justify-content-center">
+        <div style={{width: '100%', maxWidth: '400px'}}>
+            <Card className="mb-3 shadow-lg border-0 statistikaPanel">
+                <Card.Body className="text-center">
+                    <p className="text-white">Kategorije</p>
+                    <div className="statistikaText">
+                        {animatedKategorije}
+                    </div>
+                </Card.Body>
+
+            </Card>
+            <Card className="mb-3 shadow-lg border-0 statistikaPanel">
+                <Card.Body className="text-center">
+                    <p className="text-white">Slatkiši</p>
+                    <div className="statistikaText">
+                        {animatedSlatkisi}
+                    </div>
+                </Card.Body>
+            </Card>
+
+
+
+        </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </Col>
+</Row>
      
     
     </>
